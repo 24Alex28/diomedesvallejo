ae1ca458a9043d710e369f8784adc8c1d00c63a2
24Alex28 <01davp@gmail.com>
2025-08-20 20:33:49 -0400

    preformance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+export const metadata: Metadata = {
+  title: "Diomedes Vallejo",
+  description: "Developer portfolio"
+};
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,10 +16,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Diomedes Vallejo",
-  description: "Developer portfolio"
-};
+
 
 export default function RootLayout({
   children,
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head/>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Comfortaa, Zen_Loop } from "next/font/google";
-import { BranchesOutlined, GithubFilled, LinkedinFilled, MailFilled } from "@ant-design/icons";
+import { GithubFilled, LinkedinFilled, MailFilled } from "@ant-design/icons";
 import Link from "next/link";
 import { aboutMe, projects, stack } from "@/utils";
 
@@ -17,9 +17,9 @@ export default function Home() {
   return (
     <div style={{ backgroundColor: "#1a1a1a", ...font.style, }}>
       <div style={{ display: "flex", flexDirection: "row", alignItems: "flex-start", justifyContent: "flex-start", height: "100vh", flexWrap: "wrap", color: "#cacaca", flex: 1 }} className={styles.container}>
-        <Image style={{ height: '100vh', width: "100%", minWidth: "450px", maxWidth: "600px", objectFit: "cover" }} width={700} height={900} src={'/heroImage.jpeg'} alt="Diomedes Valllejo" />
+        <Image priority style={{ height: '100vh', width: "100%", minWidth: "450px", maxWidth: "600px", objectFit: "cover" }} width={700} height={900} src={'/heroImage.jpeg'} alt="Diomedes Valllejo" />
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%", flex: 1, gap: 35, top: 0, zIndex: 2, backgroundColor: "rgba(0, 0, 0, 0.8)", padding: 10, }}>
-          <Image width={60} height={80} src={'/bulb.gif'} alt="bulb light" />
+          <Image width={60} height={60} src={'/bulb.gif'} alt="bulb light" />
           <h1 style={{ fontSize: 'clamp(30px,20vw,90px)', textAlign: "center", color: "white" }} className={font_1.className}>DO YOU HAVE AN IDEA?</h1>
           <h3 style={{ fontSize: 20, width: "70%", minWidth: 320, textAlign: "center", fontWeight: "normal" }}>Lets make it happen! my name is <strong style={{ fontWeight: "bold", color: "white" }}>Diomedes Vallejo</strong> and I have been developing people’s dreams and ideas since <strong style={{ fontWeight: "bold", color: "white" }}>2020</strong>.</h3>
           <h3 style={{ fontSize: 20, width: "70%", minWidth: 320, textAlign: "center" }}>Whether you are <strong style={{ fontWeight: "bold", color: "white" }}>starting</strong> your project or want to <strong style={{ fontWeight: "bold", color: "white" }}>level up</strong>. Im here to take it where it has to be taken.</h3>
@@ -31,7 +31,7 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.section} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", flex: 1, gap: 35, top: 0, zIndex: 1, backgroundColor: "rgba(0, 0, 0, 0.8)", padding: 10, position: "inherit" }} >
-        <Image style={{ textAlign: "center", alignSelf: "center", justifySelf: "center" }} width={60} height={80} src={'/bulb.gif'} alt="bulb light" />
+        <Image style={{ textAlign: "center", alignSelf: "center", justifySelf: "center" }} width={60} height={60} src={'/bulb.gif'} alt="bulb light" />
         <h1 style={{ fontSize: 'clamp(30px,20vw,90px)', textAlign: "center", color: "white" }} className={font_1.className}>Something you should know  <span style={{ fontWeight: "bold" }}>about me</span> </h1>
         <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", alignItems: "center", justifyContent: "center", gap: 40 }}>
 
@@ -44,7 +44,7 @@ export default function Home() {
       </div>
 
       <div className={styles.section} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", flex: 1, gap: 35, top: 0, zIndex: 1, backgroundColor: "rgba(0, 0, 0, 0.8)", padding: 10, position: "inherit" }} >
-        <Image style={{ textAlign: "center", alignSelf: "center", justifySelf: "center" }} width={60} height={80} src={'/bulb.gif'} alt="bulb light" />
+        <Image style={{ textAlign: "center", alignSelf: "center", justifySelf: "center" }} width={60} height={60} src={'/bulb.gif'} alt="bulb light" />
         <h1 style={{ fontSize: 'clamp(30px,20vw,90px)', textAlign: "center", color: "white" }} className={font_1.className}><span style={{ fontWeight: "bold" }}>Once</span> ideas, <span style={{ fontWeight: "bold" }}>Now</span>  real projects</h1>
         <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", alignItems: "center", justifyContent: "center", gap: 40 }}>
 
@@ -58,14 +58,14 @@ export default function Home() {
                   <span key={i} style={{ fontSize: 10 }}>{icon}</span>
                 ))}
               </div>
-              {projectLink ? <a className={styles.button} href={projectLink}>View Project</a> : <a className={styles.button}>Private project</a>}
+              {projectLink ? <a className={styles.button} href={projectLink}>View Project</a> : <button className={styles.button}>Private project</button>}
 
             </div>)}
         </div>
       </div>
 
       <div className={styles.section} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", flex: 1, gap: 35, top: 0, zIndex: 1, backgroundColor: "rgba(0, 0, 0, 0.8)", padding: 10, position: "inherit" }} >
-        <Image style={{ textAlign: "center", alignSelf: "center", justifySelf: "center" }} width={60} height={80} src={'/bulb.gif'} alt="bulb light" />
+        <Image style={{ textAlign: "center", alignSelf: "center", justifySelf: "center" }} width={60} height={60} src={'/bulb.gif'} alt="bulb light" />
         <h1 style={{ fontSize: 'clamp(30px,20vw,90px)', textAlign: "center", color: "white" }} className={font_1.className}><span style={{ fontWeight: "bold" }}>Right</span> tools, <span style={{ fontWeight: "bold" }}>Faster </span>deployment</h1>
         <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", alignItems: "center", justifyContent: "center", gap: 40 }}>
 
@@ -85,8 +85,8 @@ export default function Home() {
 
 
       <div className={styles.section} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", flex: 1, gap: 35, top: 0, zIndex: 1, backgroundColor: "rgba(0, 0, 0, 0.8)", padding: 10, position: "inherit" }} >
-        <Image style={{ textAlign: "center", alignSelf: "center", justifySelf: "center" }} width={60} height={80} src={'/bulb.gif'} alt="bulb light" />
-        <h1 style={{ fontSize: 'clamp(30px,20vw,90px)', textAlign: "center", color: "white" }} className={font_1.className}>Still thinking about it? <span style={{ fontWeight: "bold" }}>let's talk</span></h1>
+        <Image style={{ textAlign: "center", alignSelf: "center", justifySelf: "center" }} width={60} height={60} src={'/bulb.gif'} alt="bulb light" />
+        <h1 style={{ fontSize: 'clamp(30px,20vw,90px)', textAlign: "center", color: "white" }} className={font_1.className}>Still thinking about it? <span style={{ fontWeight: "bold" }}>let&apos;s talk</span></h1>
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: 40 }}>
           <Image style={{ objectFit: "cover", borderRadius: "100%" }} width={260} height={260} src={'/heroImage.jpeg'} alt="Diomedes Valllejo" />
         </div>
